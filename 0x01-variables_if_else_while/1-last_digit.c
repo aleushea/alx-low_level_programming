ab3d411319d96bb4227ee8894d5149c9f5b96add
aleushea <aleushea@gmail.com>
2022-12-11 11:18:05 -0800

    This script will prints the alphabet in lowercase

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,18 +1,9 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
-/**
- * main - Prints the last digit of a randomly generated number
-*      and whether it is greater than 5, less than 6, or 0.
- */
-
 int main(void)
-
 {
-
 	int n;
 
 	srand(time(0));
@@ -46,9 +37,6 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n",
 
 			n, n % 10);
-
 	}
-
 	return (0);
-
 }
