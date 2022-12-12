39555eea116440a5b234b2d14666062ce1a2da83
aleushea <aleushea@gmail.com>
2022-12-12 03:08:08 -0800

    This script will prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,44 @@
+/*
+ * File: 10-print_comb2.c
+ */
+
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 00 to 99, numbers separated by
+ *        , followed by a space, in ascending order.
+ *        a comma followed by a space, in ascending order.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int digit1 = 0;
+	int digit2;
+
+	while (digit1 < 10)
+
+	for (digit1 = 0; digit1 < 10; digit1++)
+	{
+		for (digit2 = 0; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+			if (digit1 == 9 && digit2 == 9)
+			{
+				break;
+			}
+			else
+			{
+				putchar(44);
+				putchar(32);
+			}
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+		digit1++;
+	}
+	putchar('\n');
+	return (0);
+}
