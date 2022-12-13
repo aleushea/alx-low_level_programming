c2889fd4629048f64d57748635f429e6ecd5b1fb
aleushea <aleushea@gmail.com>
2022-12-12 19:09:56 -0800

    This script will prints prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,35 @@
+/*
+ * File: 4-print_alphabt.c
+ */
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: 0 if successful, non-zero otherwise.
+ */
+
+int main(void)
+
+{
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
+
+		if (letter != 'e' && letter != 'q')
+
+			putchar(letter);
+
+	}
+	putchar('\n');
+	return (0);
+}
