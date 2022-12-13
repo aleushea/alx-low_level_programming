a0368f1b3dc5b443edff6e1aa4068108dd923cf8
aleushea <aleushea@gmail.com>
2022-12-12 20:16:17 -0800

    This script will print all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,25 +9,39 @@ int main(void)
 {
 	int num = 0;
 
-	while (1)
-
 	for (num = 0; num <= 9; num++)
+
 	{
+
 		putchar((num % 10) + '0');
+
 		if (num == 9)
+
 		{
+
 			break;
+
 		}
+
 		else
+
 		{
+
 			putchar(44);
+
 			putchar(32);
+
 		}
+
 		num++;
+
 			continue;
+
 		putchar(',');
 		putchar(' ');
+
 	}
+
 	putchar('\n');
 	return (0);
 }
