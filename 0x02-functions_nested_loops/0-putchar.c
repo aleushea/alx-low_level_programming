a9fbe133ab9247528a9c063e77803768ffe41a1e
aleushea <aleushea@gmail.com>
2022-12-14 00:41:54 -0800

    This script prints putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0-putchar.c
+ *
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+
+	printf("_putchar");
+
+	putchar('\n');
+
+	return (0);
+
+}
