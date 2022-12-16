853500343177f59f6547c724557df5968452daaa
aleushea <aleushea@gmail.com>
2022-12-16 05:59:07 -0800

    This script prints a square, followed by a new line

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,15 +7,16 @@
 */
 
 void print_square(int size)
-
-int  col, row;
+{
+int col, row;
 if (size <= 0)
 {
+
 _putchar('\n');
 }
 else
 {
-for ( col = 1;  col <= size;  col++)
+for (col = 1; col <= size; col++)
 {
 _putchar('#');
 for (row = 2; row <= size; row++)
@@ -23,7 +24,6 @@ for (row = 2; row <= size; row++)
 _putchar('#');
 }
 _putchar('\n');
-
 }
 }
 }
