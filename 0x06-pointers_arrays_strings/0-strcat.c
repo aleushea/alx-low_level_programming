257fbb93788f688fa9f9fcc1eec1170995047757
aleushea <aleushea@gmail.com>
2022-12-21 09:42:46 -0800

    This script appends the src string to the dest string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,39 @@
+/**
+* _strcat - Concatenates 2 strings
+* @dest:destination char string
+* @src:The source char string
+* Return:Pointing char to the resulting string dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+char *s = dest;
+while (*dest != '\0')
+	{
+	dest++;
+	}
+while (*src != '\0')
+	{
+	*dest = *src;
+	dest++;
+	src++;
+	}
+*dest = '\0';
+return (s);
+}
+option 2
+char *_strcat(char *dest, char *src)
+{
+int ltd = 0, n;  /* @ltd: length to destination*/
+while (dest[ltd])
+	{
+	ltd++;
+	}
+for (n = 0; src[n] != 0; n++)
+	{
+	dest[ltd] = src[n];
+	ltd++;
+		}
+dest[ltd] = '\0';
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This Project 0x06.is about pointers arrays and strings
