26ec66f54e6529f5cba49953bc5b98897c66aa2c
aleushea <aleushea@gmail.com>
2022-12-30 19:31:44 -0800

    This script prints 10 times the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10(void) - Prints 10 times the alphabet, in lowercase.
+ * print_alphabet_x10(void)- Prints 10 times the alphabet, in lowercase.
  */
 
 void print_alphabet_x10(void)
