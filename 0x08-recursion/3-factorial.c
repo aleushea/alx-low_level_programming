c6cc8303c9e89bea2c5a245013a01b053b586db4
aleushea <aleushea@gmail.com>
2023-01-04 02:45:51 -0800

    This script returns the factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* factorial - This script returns the factorial of a given number
+* @n: input
+* Return: Always 0 (Success)
+*/
+
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+else if (n == 0)
+return (1);
+else
+return (n * factorial(n - 1));
+}
