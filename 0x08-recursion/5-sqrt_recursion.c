6d1317fd8bb89ac06aa1646d105cc7a719127503
aleushea <aleushea@gmail.com>
2023-01-04 10:13:30 -0800

    This script returns the natural square root of a number.

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+int squareroot(int n, int i);
+/**
+* _sqrt_recursion - this function - checks for perfect square
+* @n: input
+* Return: Always 0 (Success)
+*/
+
+int _sqrt_recursion(int n)
+
+{
+if (n < 0)
+return (-1);
+else
+return (squareroot(n, (n + 1) / 2));
+}
+
+/**
+* squareroot - checks if perfect square
+* @n: input
+* @i: counter
+* Return: returns the natural square root of a number.
+*/
+
+int squareroot(int n, int i)
+
+{
+if (i < 1)
+return (-1);
+else if (i * i == n)
+return (i);
+else
+return (squareroot(n, i - 1));
+}
