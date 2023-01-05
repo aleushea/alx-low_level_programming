61ef531b3bd2c3481119d670599f6a5934fae162
aleushea <aleushea@gmail.com>
2023-01-04 18:02:31 -0800

    This script compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,13 +2,25 @@
 #include "main.h"
 
 /**
-* wildcmp - Entry Point
-* @s1: input
-* @s2: input
-* Return: 0
+* wildcmp - this function Compares 2 strings returns 1 if they are identical
+* @s1: Input Char or String being compared
+* @s2: Input Char or String being compared that can contain special character *
+*
+* Return: 1 if considered identical, 0 otherwise
 */
 
 int wildcmp(char *s1, char *s2)
 {
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '*')
+return (wildcmp(s1, s2 + 1));
+if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+return (1);
+}
 return (0);
 }
