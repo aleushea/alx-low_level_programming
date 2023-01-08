a5c076c29fd36492ba435901d49be39b09fd4df4
aleushea <aleushea@gmail.com>
2023-01-08 09:10:03 -0800

    This script print a number, followed by a new line

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,13 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-int i;
-if (argc == 1)
+argv = argv;
 printf("%d\n", argc - 1);
-else
-{
-for (i = 0; *argv; i++, argv++)
-printf("%d\n", i - 1);
-}
 return (0);
 }
