3f49fcde7f17b9c27db546e62bec57451ceae7eb
aleushea <aleushea@gmail.com>
2023-01-08 06:33:32 -0800

    This script print the new name, without having to compile it again

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - will print the new name, without having to compile it again
+* @argc: The quantity of command line arguments
+* @argv: The list of array
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+
+{
+printf("%s\n", argv[argc * 0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This Project 0x0A-argc_argv is about arguments passed to a program
