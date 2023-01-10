3cec1c737e82e255b9caddbda9148a05ea936c4c
aleushea <aleushea@gmail.com>
2023-01-10 03:54:13 -0800

    This script will prints the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,25 +1,17 @@
-/**
-* File: 2-print_alphabet.c
-* this script is from me
-*/
 #include <stdio.h>
 /**
-* Main - function
-* 
-* Return: 0 if successful, non-zero otherwise.
+* main - Prints the sum of even-valued Fibonacci sequence
+*        terms not exceeding 4000000.
+*
 * Return: Always 0.
 */
+
 int main(void)
+
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-
-		putchar(letter);
-
-
-	putchar('\n');
-
-	return (0);
-
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
