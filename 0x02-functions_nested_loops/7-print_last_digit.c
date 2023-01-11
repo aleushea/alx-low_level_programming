6f5a15bafc92c0ae447bb8ca4d68a2742ba9800d
aleushea <aleushea@gmail.com>
2023-01-10 22:37:08 -0800

    This script prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,14 @@
 * print_last_digit - Prints the last digit of a number.
 * print_last_digit - prints
 *
-* @n: - The number in question.
+* @int: the dataa type
 * Return: Value of the last digit.
 */
 
-int print_last_digit(int n)
+int print_last_digit(int)
 {
 if (n < 0)
 n = -n;
-_putchar((n % 10) + '0');
+_putchar(n % 10);
 return (n % 10);
 }
