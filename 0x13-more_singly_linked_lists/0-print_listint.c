bc4fda2dce6afbbe30f19241fdb87fb9b7e80ffc
aleushea <aleushea@gmail.com>
2023-01-30 10:22:18 -0800

    This scripts performs Print list List length Add node and Add node at the end

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t stored_data = 0;
+while (h)
+	{
+	printf("%d\n", h->n);
+	stored_data++;
+	h = h->next;
+	}
+return (stored_data);
+}
