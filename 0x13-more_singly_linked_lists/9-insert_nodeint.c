c5236e5d5bcd086630b32a4ea453f7aefc8eb992
aleushea <aleushea@gmail.com>
2023-01-31 12:56:23 -0800

    This script inserts a new node to a listint_t linked list at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,49 +1,39 @@
 #include "lists.h"
-
 /**
- * insert_nodeint_at_index - a function that inserts
- * a new node at a given position.
- * @head: the head.
- * @idx: index of list.
- * @n: the integer.
- * Return: nodes
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int i = 0;
-listint_t *node, *node_temporal;
-
-if (!head && !*head)
+unsigned int i;
+listint_t *new;
+listint_t *temp = *head;
+new = malloc(sizeof(listint_t));
+if (!new || !head)
 return (NULL);
-node_temporal = *head;
-if (idx == i)
+new->n = n;
+new->next = NULL;
+if (idx == 0)
 {
-node = malloc(sizeof(listint_t));
-if (!node)
-return (NULL);
-node->n = n;
-node->next = *head;
-*head = node;
-return (node);
+new->next = *head;
+*head = new;
+return (new);
+}
+for (i = 0; temp && i < idx; i++)
+{
+if (i == idx - 1)
+{
+new->next = temp->next;
+temp->next = new;
+return (new);
 }
 else
-{
-while (node_temporal)
-{
-if (idx == i + 1)
-{
-node = malloc(sizeof(listint_t));
-if (!node)
-return (NULL);
-node->n = n;
-node->next = node_temporal->next;
-node_temporal->next = node;
-return (node);
-}
-node_temporal = node_temporal->next;
-i++;
-}
+temp = temp->next;
 }
 return (NULL);
 }
