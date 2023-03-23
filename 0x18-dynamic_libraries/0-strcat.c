343c19831f59ab9fa7d7e7f4df69f11ff88683bc
aleushea <aleushea@gmail.com>
2023-03-23 06:51:22 -0700

    first project 0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strcat - Concatenates 2 strings
+* @dest:destination char string
+* @src:The source char string
+* Return:Pointing char to the resulting string dest
+*/
+
+char *_strcat(char *dest, char *src)
+
+{
+int ltd = 0, n;  /* @ltd: length to destination*/
+while (dest[ltd])
+	{
+	ltd++;
+	}
+for (n = 0; src[n] != 0; n++)
+	{
+	dest[ltd] = src[n];
+	ltd++;
+		}
+dest[ltd] = '\0';
+return (dest);
+}
