584f18ffd57f187b94852abea984a62c4cbc3e09
aleushea <aleushea@gmail.com>
2023-08-18 20:07:51 -0700

    revised

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -27,7 +27,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		start->prev = NULL;
 		return (1);
 	}
-	for (i = 0; i <= index - 1; i++)
+	for (i = 0; i < index; i++)
 	{
 		start = start->next;
 		if (!start)
